531d286b5059a3c28e6a4cbc6e49ad92701701e2
=NeveenElattar <=neveenelattar7@gmail.com>
2018-04-17 12:45:41 +0200

    Throw Sphere (as a prefab) from random lanes positions

---
+++ Assets/Game/Scripts/Minions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minions : MonoBehaviour {
+
+    public GameObject[] lanes;
+    int randNum;
+   // Rigidbody rb;
+    public Rigidbody pref;
+
+
+	void Start () {
+        StartCoroutine(Throw());
+	}
+	
+	IEnumerator Throw()
+    {
+        while (true)
+        {
+            Rigidbody clone;
+            randNum = Random.Range(0, lanes.Length - 1);
+            clone = Instantiate(pref, lanes[randNum].transform.position, Quaternion.identity) as Rigidbody;
+            clone.velocity = new Vector3(0, 0, -20.0f);
+            clone.tag = "Minion";
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
