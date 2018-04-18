bcd3c8f432d8457d1d27fb8c8fe8d628fc67caec
=NeveenElattar <=neveenelattar7@gmail.com>
2018-04-18 18:51:36 +0200

    Sphere colliders

---
+++ Assets/Game/Scripts/Scripts/SphereControll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControll : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sword")
+        {
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
