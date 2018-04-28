9c1f945e637b2b0fa8efb6691d09412d57f3934f
=NeveenElattar <=neveenelattar7@gmail.com>
2018-04-28 10:35:04 +0200

    StateMachine Class and interface without states

---
+++ Assets/Game/Scripts/Interfaces/IState.cs
@@ -0,0 +1,6 @@
+﻿public interface IState
+{
+    void Enter();
+    void Excute();
+    void Exit();
+}
---
+++ Assets/Game/Scripts/Scripts/StateMachine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour {
+
+    IState currentState;
+    IState previousState;
+
+	public void changeState(IState newState)
+    {
+        if(currentState != null)
+        {
+            currentState.Exit();
+        }
+        previousState = currentState;
+        currentState = newState;
+        currentState.Enter();
+    }
+    public void stateUpdate()
+    {
+        var runningState = currentState;
+        if (runningState != null)
+        {
+            runningState.Excute();
+        }
+    }
+    public void switchToPreviousState()
+    {
+        currentState.Exit();
+        currentState = previousState;
+        currentState.Enter();
+    }
+}
