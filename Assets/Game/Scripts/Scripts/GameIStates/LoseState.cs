69b0620ceb5450cfab18ebf9f5c24c0ebf5e7dff
= <moh_ussama@hotmail.com>
2018-04-30 16:42:23 +0200

    lose state

--- Assets/Game/Scripts/Scripts/GameIStates/LoseState.cs
+++ Assets/Game/Scripts/Scripts/GameIStates/LoseState.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoseState : MonoBehaviour {
+public class LoseState : IState
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    StateMachine stateMachine; GameManager gameManager;
+
+
+    public void Enter(StateMachine stateMachine, GameManager gameManager)
+    {
+        this.stateMachine = stateMachine;
+        this.gameManager = gameManager;
+    }
+
+    public void Excute()
+    {
+        Debug.Log(" i'm in lose scene ");
+    }
+
+    public void Exit()
+    {
+        throw new NotImplementedException();
+    }
+
+    
 }
