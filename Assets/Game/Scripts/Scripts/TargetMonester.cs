e624ae4b873f451fecd631e111fa2e8ff6e4832a
=NeveenElattar <=neveenelattar7@gmail.com>
2018-04-30 17:52:11 +0200

    Target health

--- Assets/Game/Scripts/Scripts/TargetMonester.cs
+++ Assets/Game/Scripts/Scripts/TargetMonester.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetMonester : MonoBehaviour {
 
-    public float health = 50f;
+    public float health = 100f;
+    Scrollbar bar;
 
+    private void Start()
+    {
+        bar = GameObject.FindObjectOfType<Scrollbar>();
+        bar.size = 1;
+    }
 
-	public void TakeDamage(float amount)
+    private void Update()
+    {
+        bar.transform.position = transform.position;
+    }
+
+    public void TakeDamage(float amount)
     {
         health -= amount;
+        bar.size -= 1 / amount;
         if (health <= 0)
         {
             Die();
