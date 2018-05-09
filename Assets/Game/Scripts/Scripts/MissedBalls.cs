363239b81dc089cc78f5b6d6529ff3bfec6735cb
Alaa <alaagamie@yahoo.com>
2018-05-09 18:50:23 +0200

    change layers

--- Assets/Game/Scripts/Scripts/MissedBalls.cs
+++ Assets/Game/Scripts/Scripts/MissedBalls.cs
@@ -9,7 +9,7 @@ public class MissedBalls : MonoBehaviour {
     public GameEvent noteMissedE;
     public GameEvent LoseE;
 
-    private void OnTriggerEnter(Collider collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Note")
         {
@@ -18,6 +18,7 @@ public class MissedBalls : MonoBehaviour {
                     playerHealth.value -= 4;
             }
             else
+
             {
                 LoseE.Raise();
             }
