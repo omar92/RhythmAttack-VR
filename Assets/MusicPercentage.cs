9ec83fedf9a31d2d8cf8f8a1a9ca027ed9b60040
= <moh_ussama@hotmail.com>
2018-05-20 18:02:36 +0200

    fix music bar

---
+++ Assets/MusicPercentage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPercentage : MonoBehaviour {
+
+    public FloatVariable percentage;
+    
+    
+    AudioSource source;
+    AudioClip clip;
+	void Start () {
+        source = GetComponent<AudioSource>();
+        clip = source.clip;
+	}
+	
+	
+	void Update () {
+
+        percentage.value = Time.time / clip.length;
+        
+	}
+}
