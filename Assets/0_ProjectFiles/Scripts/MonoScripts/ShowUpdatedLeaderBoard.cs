f91814bd5aa4703a8fa169e3610782d4259927e9
Alaa <alaagamie@yahoo.com>
2018-05-21 10:59:23 +0200

    LB

--- Assets/0_ProjectFiles/Scripts/MonoScripts/ShowUpdatedLeaderBoard.cs
+++ Assets/0_ProjectFiles/Scripts/MonoScripts/ShowUpdatedLeaderBoard.cs
@@ -16,7 +16,7 @@ public class ShowUpdatedLeaderBoard : MonoBehaviour
         {
             transform.GetChild(i+1).gameObject.SetActive(true);
             transform.GetChild(i + 1).GetComponentsInChildren<Text>()[1].text = dictionary.Value.Keys.ElementAt(i).ToString();
-            transform.GetChild(i + 1).GetComponentsInChildren<Text>()[1].text = dictionary.Value.Values.ElementAt(i).ToString();
+            transform.GetChild(i + 1).GetComponentsInChildren<Text>()[0].text = dictionary.Value.Values.ElementAt(i).ToString();
         }
     }
 }
