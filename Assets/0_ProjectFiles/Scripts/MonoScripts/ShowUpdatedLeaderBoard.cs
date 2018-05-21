5c6429e84e186342c027735b3719a2aab4aac88b
=NeveenElattar <=neveenelattar7@gmail.com>
2018-05-21 10:12:57 +0200

    LB Fix

--- Assets/0_ProjectFiles/Scripts/MonoScripts/ShowUpdatedLeaderBoard.cs
+++ Assets/0_ProjectFiles/Scripts/MonoScripts/ShowUpdatedLeaderBoard.cs
@@ -15,7 +15,8 @@ public class ShowUpdatedLeaderBoard : MonoBehaviour
         for (int i = 0; i < Mathf.Min(dictionary.Value.Count, transform.childCount); i++)
         {
             transform.GetChild(i+1).gameObject.SetActive(true);
-            transform.GetChild(i+1).GetComponentsInChildren<Text>()[1].text = dictionary.Value.Keys.ElementAt(i).ToString();
+            transform.GetChild(i + 1).GetComponentsInChildren<Text>()[1].text = dictionary.Value.Keys.ElementAt(i).ToString();
+            transform.GetChild(i + 1).GetComponentsInChildren<Text>()[1].text = dictionary.Value.Values.ElementAt(i).ToString();
         }
     }
 }
