00b4d75549ffa20a40188f9502a47e6915f333a7
= <moh_ussama@hotmail.com>
2018-05-22 14:21:19 +0200

    Pause Menu

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public GameEvent pauseEvent;
+    public GameEvent unPauseEvent;
+
+    public GameObject bgMusic;
+    AudioSource bgSource;
+
+    [SerializeField] private GameObject pausePanel;
+    void Start()
+    {
+        bgSource = bgMusic.GetComponent<AudioSource>();
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("pause the game");
+            if (!pausePanel.activeInHierarchy)
+            {
+                pauseEvent.Raise();
+            }
+            
+        }
+        //if (Input.GetKeyDown(KeyCode.L))
+        //{
+        //    ContinueGame();
+        //    // 
+        //}
+    }
+    public void Pause()
+    {
+        bgSource.Pause();
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    public void ContinueGame()
+    {
+        bgSource.Play();
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }
+}
