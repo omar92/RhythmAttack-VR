81b84060a728e299ab46f385c66a0e2548e36fad
Alaa <alaagamie@yahoo.com>
2018-05-22 18:38:47 +0200

    pause menu

--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 public class PauseGame : MonoBehaviour
 {
 
-    public GameEvent pauseEvent;
+    //public GameEvent pauseEvent;
 
-    public GameObject bgMusic;
-    AudioSource bgSource;
+    //public GameObject bgMusic;
+   // AudioSource bgSource;
 
-    [SerializeField] private GameObject pausePanel;
+   // [SerializeField] private GameObject pausePanel;
     void Start()
     {
-        bgSource = bgMusic.GetComponent<AudioSource>();
+      //  bgSource = bgMusic.GetComponent<AudioSource>();
 
     }
     void Update()
     {
 
         //Debug.Log("pause the game");
-        if (false )
-        {
-            pauseEvent.Raise();
-        }
+        //if (false )
+        //{
+        //    pauseEvent.Raise();
+        //}
             
        
     }
     public void Pause()
     {
-        bgSource.Pause();
-        Time.timeScale = 0;
-        pausePanel.SetActive(true);
+       // bgSource.Pause();
+      //  Time.timeScale = 0;
+      //  pausePanel.SetActive(true);
         //Disable scripts that still work while timescale is set to 0
     }
     public void ContinueGame()
     {
-        bgSource.Play();
-        Time.timeScale = 1;
-        pausePanel.SetActive(false);
+       // bgSource.Play();
+     //   Time.timeScale = 1;
+      //  pausePanel.SetActive(false);
         //enable the scripts again
     }
 }
