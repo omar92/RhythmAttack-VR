dec81fc9c91835d2b3bdaee1b880780d286c1931
Alaa <alaagamie@yahoo.com>
2018-06-02 20:53:03 +0200

    fix intersiction

--- Assets/Let's Try Shooter/Scenes/Prefab/SwordIntersiction.cs
+++ Assets/Let's Try Shooter/Scenes/Prefab/SwordIntersiction.cs
@@ -5,27 +5,36 @@ using UnityEngine;
 public class SwordIntersiction : MonoBehaviour {
 
     public ParticleSystem intersictionParticle;
-
+    //public int layerMask =1<<13;
+    public int myMask ;
     private void Start()
     {
         //intersictionParticle=Instantiate(intersictionParticle.gameObject,Vector3.zero,Quaternion.identity);
         intersictionParticle.gameObject.SetActive(false);
+        
     }
     private void Update()
     {
+        myMask = 1 << 12;
         RaycastHit hit;
-        if (Physics.SphereCast(transform.position, .057f, Vector3.Cross(transform.up, transform.right), out hit, 1.2f))
+        if (Physics.SphereCast(transform.position, .057f, Vector3.Cross(transform.up, transform.right), out hit, 1.2f,myMask))
         {
             if (hit.transform.tag == "Blade")
             {
-
+                Debug.Log("fooooooooooooooool");
                 intersictionParticle.transform.position = hit.point;
                 intersictionParticle.gameObject.SetActive(true);
             }
             else
             {
                 intersictionParticle.gameObject.SetActive(false);
+                
             }
+
+        }
+        else
+        {
+            intersictionParticle.gameObject.SetActive(false);
 
         }
 
