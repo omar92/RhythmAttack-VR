f3a41d4629a4c40ffdada6a299ab2d1dbb06c5f3
ENG_OMARSLEAM\omar <ooomar92@gmail.com>
2018-06-06 17:16:37 +0200

    Boss animation handling

---
+++ Assets/0_ProjectFiles/Scripts/ScriptableObjectsScripts/BooleanAnimationSetter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BooleanAnimationSetter : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+    }
+	
+    public void SetTrue(string key)
+    {
+        anim.SetBool(key,true);
+    }
+
+    public void SetFalse(string key)
+    {
+        anim.SetBool(key, false);
+    }
+
+    public void SetTrue(int index)
+    {
+        anim.SetBool(index, true);
+    }
+
+    public void SetFalse(int index)
+    {
+        anim.SetBool(index, false);
+    }
+}
