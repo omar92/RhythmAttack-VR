aeb527dbfb192c56f421e4dcccab23afe9239161
=NeveenElattar <=neveenelattar7@gmail.com>
2018-06-06 10:52:13 +0200

    Json

--- Assets/0_ProjectFiles/Scripts/MonoScripts/UIStaff/JsonManager.cs
+++ Assets/0_ProjectFiles/Scripts/MonoScripts/UIStaff/JsonManager.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class JsonManager : MonoBehaviour {
-
+public class JsonManager : MonoBehaviour
+{
     public FloatVariable finalScore;
     public int maxNumOfBoard = 10;
     float newScore = -1f;
@@ -14,49 +14,29 @@ public class JsonManager : MonoBehaviour {
     public ListVariable playerList;
     public StringVariable playerName;
 
-
-    //void Awake()
-    //{
-    //    //if (PlayerPrefs.GetString("scoreJson") == null)
-    //    //{
-    //    //    playerList = new List<Player>();
-    //    //}else
-    //    //{
-    //     //   playerList = JsonUtility.FromJson<ListVariable>(PlayerPrefs.GetString("scoreJson"));
-    //    //}
-
-    //} 
-
     public void SaveJson()
     {
 
         if (playerList.list.Count != 0 || playerList.list.Count < maxNumOfBoard)
         {
-            //if (finalScore.value != newScore)
-            //{
-                player = new Player(finalScore.value , playerName.value);
-                playerList.list.Add(player);
-            Debug.Log("D5al fl ola ahooo");
-            //}
+            if (playerName.value.Length <= 0)
+                playerName.value = "Player";
+            player = new Player(finalScore.value, playerName.value);
+            playerList.list.Add(player);
         }
         else
         {
-            Debug.Log("D5al fl else ahooo");
-            if (finalScore.value > playerList.list[playerList.list.Count-1].playerScore)
+            if (playerName.value.Length <= 0)
+                playerName.value = "Player";
+            if (finalScore.value > playerList.list[playerList.list.Count - 1].playerScore)
             {
                 playerList.list[playerList.list.Count - 1].playerScore = finalScore.value;
                 playerList.list[playerList.list.Count - 1].playerName = playerName.value;
-            }            
+            }
         }
-        //newScore = finalScore.value;
         playerList.list.Sort();
         playerList.list.Reverse();
-        foreach (var item in playerList.list)
-        {
-            Debug.Log("Listaaa Name"+ item.playerName+ "Listaaa Score" + item.playerScore);
-        }
         json = JsonUtility.ToJson(playerList);
-        //PlayerPrefs.SetString("scoreJson", json);
         LeaderboardChanged.Raise();
         playerName.value = "Player";
     }
