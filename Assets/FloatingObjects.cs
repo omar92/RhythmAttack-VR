159139c00233ea776a80aa34fa18d0296da16e55
= <moh_ussama@hotmail.com>
2018-06-11 14:36:14 +0200

    floating object script

---
+++ Assets/FloatingObjects.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObjects : MonoBehaviour {
+
+
+    
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f)/*, Space.World*/);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime *3 * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
