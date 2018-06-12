f6a3d9c863c46be9dbcf6da6289fc571d3347cf2
ENG_OMARSLEAM\omar <ooomar92@gmail.com>
2018-06-12 13:31:53 +0200

    animator updated

---
+++ Assets/SetFloatVariableToAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFloatVariableToAnim : MonoBehaviour
+{
+
+
+    public FloatVariable var;
+
+
+    Animator anim;
+    // Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void SetAnim(string key)
+    {
+        anim.SetInteger(key, (int)var.value);
+    }
+
+    public void SetFalse(int index)
+    {
+        anim.SetInteger(index, (int)var.value);
+    }
+}
