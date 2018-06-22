7213b4b7f99271b3557f0c27d7ba1e0b43fd5615
AhmadMostafa <ahmed.aljazayeerly91@gmail.com>
2018-06-23 00:18:23 +0200

    ui inhanced

---
+++ Assets/LookAt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+
+    public TransformVariable target;
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(target.value);
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+}
