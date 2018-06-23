09c688f24f6d3bb87eb61fd3a0d79233f0ff4704
AhmadMostafa <ahmed.aljazayeerly91@gmail.com>
2018-06-23 17:37:40 +0200

    intro handling

---
+++ Assets/0_ProjectFiles/Scripts/MonoScripts/IntroMusicMonitor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMusicMonitor : MonoBehaviour
+{
+
+    public FloatVariable musicLengyh;
+    public GameEvent introMusicEnd;
+    Coroutine waitMusic;
+
+    public void BeginCoroutine()
+    {
+        waitMusic = StartCoroutine("MusicCoroutine");
+    }
+
+    IEnumerable MusicCoroutine()
+    {
+        while (musicLengyh.value > 0)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        introMusicEnd.Raise();
+    }
+}
